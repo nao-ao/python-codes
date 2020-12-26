152350c0221edf7a8d052e0055cd00c087157fd0
naoto aoki <72431624+nao-ao@users.noreply.github.com>
2020-12-26 21:02:19 +0900

    add twitter

--- twitter/tweet.py
+++ twitter/tweet.py
@@ -5,8 +5,6 @@ CONSUMER_SECRET = ''
 ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
 
-# 先ほど取得した各種キーを代入する
-
 # Twitterオブジェクトの生成
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -16,10 +14,14 @@ api = tweepy.API(auth)
 api.update_status("Tweet from python")
 
 # 画像付きツイート
-api.update_with_media(status = 'image tweet from python', filename = 'sample.png')
+api.update_with_media(status='image tweet from python', filename='sample.png')
 
 # リプライツイート
+
 # まずリプライを返したいユーザーのID(@のID)を代入
+# @twitter_id1なら
+# Account = 'twitter_id1'
+# と入力
 Account = ''
 
 # user_timelineで指定したユーザーのタイムラインのTweetを取得(最大20個まで)
@@ -29,18 +31,7 @@ tweets = api.user_timeline(Account, count=20, page=1)
 msg = 'reply from python'
 for tweet in tweets:
   reply_text = "@"+str(tweet.user.screen_name) +" "+ msg
+  # テキスト(メッセージ)のみ
   api.update_status(status = reply_text, in_reply_to_status_id = tweet.id)
-  # api.update_with_media('img.jpg', status = reply_text, in_reply_to_status_id = tweet.id)
-
-# 画像付きリプライツイート
-# まずリプライを返したいユーザーのID(@のID)を代入
-Account = 'samplemoviebot1'
-
-# user_timelineで指定したユーザーのタイムラインのTweetを取得(最大20個まで)
-tweets = api.user_timeline(Account, count=20, page=1)
-
-# その一つ一つに対してリプライをする．
-msg = 'reply img from python'
-for tweet in tweets:
-  reply_text = "@"+str(tweet.user.screen_name) +" "+ msg
-  api.update_with_media('sample.png', status = reply_text, in_reply_to_status_id = tweet.id)
+  # 画像付きツイート
+  api.update_with_media('img.jpg', status = reply_text, in_reply_to_status_id = tweet.id)
