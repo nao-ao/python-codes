7fceae692ce470e404c58d4b6a4486f9562eabcb
naoto aoki <72431624+nao-ao@users.noreply.github.com>
2021-01-11 08:19:50 +0900

    Update pandas_cheat_sheet.py

--- pandas/pandas_cheat_sheet.py
+++ pandas/pandas_cheat_sheet.py
@@ -5,3 +5,13 @@ empty_df = pd.DataFrame()
 print(empty_df.empty)
 # True
 
+df = pd.DataFrame({
+  "名前":['佐藤', '鈴木', '高橋'],
+  "年齢":[23, 45, 32],
+})
+
+print(df)
+#    名前  年齢
+# 0  佐藤  23
+# 1  鈴木  45
+# 2  高橋  32
