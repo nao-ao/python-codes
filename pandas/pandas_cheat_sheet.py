58bbc6dd1a34c9f96778b79042ce8b1d5706cdbe
naoto aoki <72431624+nao-ao@users.noreply.github.com>
2021-01-11 09:20:45 +0900

    Update pandas_cheat_sheet.py

--- pandas/pandas_cheat_sheet.py
+++ pandas/pandas_cheat_sheet.py
@@ -47,3 +47,12 @@ print(list_to_df)
 # 3  1
 # 4  5
 
+list_to_ic_df = pd.DataFrame(sample_list, index=['a', 'b', 'c', 'd', 'e'], columns=['column1'])
+print(list_to_ic_df)
+#    0
+# 0  3
+# 1  2
+# 2  4
+# 3  1
+# 4  5
+
